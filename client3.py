173c486452ce45f2980b96ae68d486b4a860af96
Sourabh Kushwah <uuc110@gmail.com>
2024-03-03 13:33:19 +0530

    Fixed the comment

--- client3.py
+++ client3.py
@@ -50,6 +50,8 @@ if __name__ == "__main__":
     # Query the price once every N seconds.
     for _ in iter(range(N)):
         quotes = json.loads(urllib.request.urlopen(QUERY.format(random.random())).read())
+
+        """ ----------- Update to get the ratio --------------- """
         prices = {}
         for quote in quotes:
             stock, bid_price, ask_price, price = getDataPoint(quote)
